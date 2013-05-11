c180a14daa9926d23c5d514de28d6a7815e20014
Roma Klapaukh <rklapaukh@hotmail.com>
2013-05-12 00:00:35 +1200

    Added more methods for points

--- src/Point.java
+++ src/Point.java
@@ -17,5 +17,43 @@ public class Point {
 		final double dy = o.y - this.y;
 		return Math.sqrt(dx * dx + dy*dy);
 	}
+	
+	public Point forceForwards(){
+		if(Double.compare(y,0) > 0 || (Double.compare(y, 0) == 0 && Double.compare(x,0) >= 0)){
+			return this;
+		}
+		return new Point(-x,-y);
+	}
+	
+	public double x(){
+		return this.x;
+	}
+	
+	public double y(){
+		return this.y;
+	}
+	
+	public Point minus(Point o){
+		return new Point(this.x - o.x, this.y - o.y);
+	}
+	
+	public double angleToOtherFromX(Point o){
+		return this.minus(o).forceForwards().angleToX();
+	}
+	
+	public double angleToX(){
+		//use the dot product with [1,0]
+		double dot = x;
+		double length = length();
+		if(Double.compare(length, 0) == 0){
+			return 0;
+		}
+		double norm = dot / length;
+		return Math.acos(norm);
+	}
+	
+	public double length(){
+		return Math.sqrt(x*x + y*y);
+	}
 
 }
