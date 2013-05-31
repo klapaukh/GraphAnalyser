f01366b37f365b898f96f4a3ed2bf9872d6b2c1c
Roman Klapaukh <roma@ecs.vuw.ac.nz>
2013-05-31 14:41:19 +1200

    Added new parameters to analysis

--- src/GraphAnalyser.java
+++ src/GraphAnalyser.java
@@ -45,7 +45,7 @@ public class GraphAnalyser {
 		tests.add(new Analysis.LayoutProperty(Analysis.KG));
 		tests.add(new Analysis.LayoutProperty(Analysis.WELLMASS));
 		tests.add(new Analysis.LayoutProperty(Analysis.EDGECHARGE));
-		tests.add(new Analysis.MirrorSymmetry(0.1, 10, 2, false));
+		tests.add(new Analysis.MirrorSymmetry(0.1, 2, false,4,5,5));
 
 		List<Graph> graphs = new ArrayList<>();
 		try {
