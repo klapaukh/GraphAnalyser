b0cd2e2037d586a31d5337b305cb0ffc29593009
Roma Klapaukh <rklapaukh@hotmail.com>
2013-06-01 13:14:44 +1200

    Added javadoc to Pair

--- src/analysis/Pair.java
+++ src/analysis/Pair.java
@@ -1,10 +1,30 @@
 package analysis;
 
+/**
+ * This class represents a generic immutable pair type.
+ * 
+ * @author Roma Klapaukh
+ *
+ * @param <E> Type of x
+ * @param <F> Type of y
+ */
 public class Pair<E,F> {
 
+	/**
+	 * First value
+	 */
 	public final E x;
+	
+	/**
+	 * Second value
+	 */
 	public final F y;
 
+	/**
+	 * Create a new pair with the given values
+	 * @param x The first value
+	 * @param y The second value
+	 */
 	public Pair(E x, F y){
 		this.x=x;
 		this.y=y;
