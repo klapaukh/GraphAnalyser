83b0639f9f34c818dadca78a545c1e16c5d168c8
Roma Klapaukh <rklapaukh@hotmail.com>
2013-06-01 14:38:05 +1200

    Refactoring general symmtery code out into Symmtery class

--- src/analysis/MirrorSymmetry.java
+++ src/analysis/MirrorSymmetry.java
@@ -8,19 +8,11 @@ import main.Graph;
 
 public class MirrorSymmetry extends Symmetry {
 
-
-	private final int numMirrors;
-	private final int angleMerge;
-	private final int pixelsMerge;
-
 	public MirrorSymmetry(double sigma_scale, double sigma_distance,
 			boolean useDistanceWeighting, int numMirrors, int angleMerge,
 			int pixelMerge) {
-		super(sigma_scale, useDistanceWeighting, sigma_distance);
-		
-		this.numMirrors = numMirrors;
-		this.angleMerge = angleMerge;
-		this.pixelsMerge = pixelMerge;
+		super(sigma_scale, useDistanceWeighting, sigma_distance, numMirrors,
+				angleMerge, pixelMerge);
 	}
 
 	public String toString() {
@@ -28,23 +20,7 @@ public class MirrorSymmetry extends Symmetry {
 	}
 
 	public String value(Graph g) {
-		List<SIFTFeature> edges = new ArrayList<>();
-
-		// Make all the edges into features
-		// Each feature is the center of the edge
-		for (int i = 0; i < g.numNodes(); i++) {
-			for (int j = i + 1; j < g.numNodes(); j++) {
-				if (g.isEdge(i, j)) {
-					Point n1 = g.getNode(i);
-					Point n2 = g.getNode(j);
-					double x = (n1.x() + n2.x()) / 2;
-					double y = (n1.y() + n2.y()) / 2;
-					double length = n1.distanceTo(n2);
-					double theta = n1.angleToOtherFromX(n2);
-					edges.add(new SIFTFeature(x, y, theta, length, 1, i, j));
-				}
-			}
-		}
+		List<SIFTFeature> edges = createFeatures(g);
 
 		// Now that we have the features, we need to find axes
 		List<Vote> votes = new ArrayList<>();
@@ -64,9 +40,6 @@ public class MirrorSymmetry extends Symmetry {
 							* Math.sin(thetaij);
 
 					double vote = phi * s * d;
-					if (vote > 2) {
-						System.out.println(vote);
-					}
 
 					votes.add(new Vote(rij, Math.toDegrees(thetaij), vote,
 							f1.node1, f1.node2, f2.node1, f2.node2));
@@ -74,36 +47,7 @@ public class MirrorSymmetry extends Symmetry {
 			}
 		}
 
-		double maxang = 0;
-		double maxrad = 0;
-		double minang = Double.MAX_VALUE;
-		double minrad = Double.MAX_VALUE;
-
-		for (Vote v : votes) {
-			maxang = Math.max(maxang, v.theta);
-			maxrad = Math.max(maxrad, v.rad);
-			minrad = Math.min(minrad, v.rad);
-			minang = Math.min(minang, v.theta);
-		}
-
-		// System.out.println("\n ("+minang + "," + maxang+") deg : (" +
-		// minrad+","+minrad+")");
-
-		double[][] voteSpace = new double[(int) (maxang - minang) + 1][(int) (maxrad - minrad) + 1];
-		for (Vote v : votes) {
-			voteSpace[(int) (v.theta - minang)][(int) (v.rad - minrad)] += v.vote;
-		}
-
-		// voteSpace = Image.gaussianBlur(voteSpace, sigma_gauss);
-		int numDeg = angleMerge, numPix = pixelsMerge;
-		double[][] redVoteSpace = Image.sampleDown(voteSpace, numDeg, numPix);
-
-		Image.draw(redVoteSpace, "ex.pbm");
-
-		// System.out.println("\n ("+minang + "," + maxang+") deg : (" +
-		// minrad+","+maxrad+")");
-		List<Point> axis = Image.findMax(redVoteSpace, minang, minrad,
-				10 / numDeg, 10 / numPix, numMirrors, numDeg, numPix);
+		List<Point> axis = findMaxima(votes);
 
 		try {
 			Image.drawWithMirrorLines(g, axis, votes, "temp.svg");
@@ -112,31 +56,7 @@ public class MirrorSymmetry extends Symmetry {
 			e.printStackTrace();
 		}
 
-		// compute score
-		// \sigma numEdges mirrored
-		// ----------------------
-		// numEdges * numMirrors
-		int numEdgesMirorred = 0;
-		int numEdges = 0;
-		for (int i = 0; i < g.numNodes(); i++) {
-			for (int j = i + 1; j < g.numNodes(); j++) {
-				if (g.isEdge(i, j)) {
-					numEdges++;
-					// What was my vote
-					for (Vote v : votes) {
-						if ((v.i == i && v.j == j) || (v.i2 == i && v.j2 == j)) {
-							for (Point p : axis) {
-								if (Math.abs(p.x() - v.theta) < 10
-										&& Math.abs(p.y() - v.rad) < 15) {
-									numEdgesMirorred++;
-								}
-							}
-						}
-					}
-				}
-			}
-		}
-		double score = numEdgesMirorred / (double) (axis.size() * numEdges);
+		double score = computeScore(g, axis, votes);
 		return String.format("%.4f", score);
 	}
 
@@ -144,7 +64,5 @@ public class MirrorSymmetry extends Symmetry {
 		double thetaij = f1.angleTo(f2);
 		return Math.abs(Math.cos(f1.theta + f2.theta - 2 * thetaij));
 	}
-
-
 
 }
--- src/analysis/Symmetry.java
+++ src/analysis/Symmetry.java
@@ -1,15 +1,29 @@
 package analysis;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import main.Graph;
+
 
 public abstract class Symmetry implements Analysis {
 	private final double sigma_scale;
 	private final boolean distanceBound;
 	private final double sigma_distance;
 	
-	public Symmetry(double sigma_scale, boolean distanceBound, double sigma_distance){
+	private final int numFeatures;
+	private final int xMerge;
+	private final int yMerge;
+	private final int xMin = 10;
+	private final int yMin = 10;
+	
+	public Symmetry(double sigma_scale, boolean distanceBound, double sigma_distance, int numFeatures, int xMerge, int yMerge){
 		this.sigma_scale = sigma_scale;
 		this.distanceBound = distanceBound;
 		this.sigma_distance = sigma_distance;
+		this.numFeatures = numFeatures;
+		this.xMerge = xMerge;
+		this.yMerge = yMerge;
 	}
 	
 	protected double scaleFactor(SIFTFeature f1, SIFTFeature f2) {
@@ -27,5 +41,82 @@ public abstract class Symmetry implements Analysis {
 		double d = f1.distanceTo(f2);
 		double exp = -(d * d) / (2 * sigma_distance * sigma_distance);
 		return Math.exp(exp);
+	}
+	
+	protected List<SIFTFeature> createFeatures(Graph g){
+		List<SIFTFeature> edges = new ArrayList<>();
+
+		// Make all the edges into features
+		// Each feature is the center of the edge
+		for (int i = 0; i < g.numNodes(); i++) {
+			for (int j = i + 1; j < g.numNodes(); j++) {
+				if (g.isEdge(i, j)) {
+					Point n1 = g.getNode(i);
+					Point n2 = g.getNode(j);
+					double x = (n1.x() + n2.x()) / 2;
+					double y = (n1.y() + n2.y()) / 2;
+					double length = n1.distanceTo(n2);
+					double theta = n1.angleToOtherFromX(n2);
+					edges.add(new SIFTFeature(x, y, theta, length, 1, i, j));
+				}
+			}
+		}
+		
+		return edges;
+	}
+	
+	protected List<Point> findMaxima( List<Vote> votes){
+		double maxx = 0;
+		double maxy = 0;
+		double minx = Double.MAX_VALUE;
+		double miny = Double.MAX_VALUE;
+
+		for (Vote v : votes) {
+			maxx = Math.max(maxx, v.theta);
+			maxy = Math.max(maxy, v.rad);
+			miny = Math.min(miny, v.rad);
+			minx = Math.min(minx, v.theta);
+		}
+		
+		double[][] voteSpace = new double[(int) (maxx - minx) + 1][(int) (maxy - miny) + 1];
+		for (Vote v : votes) {
+			voteSpace[(int) (v.theta - minx)][(int) (v.rad - miny)] += v.vote;
+		}
+		
+		double[][] voteRedSpace = Image.sampleDown(voteSpace, xMerge, yMerge);
+		
+		Image.draw(voteRedSpace, "ex.pbm");
+		
+		return Image.findMax(voteRedSpace, minx, miny, yMin/yMerge, xMin/xMerge, numFeatures, xMerge, yMerge);
+	}
+	
+	protected double computeScore(Graph g, List<Point> axis, List<Vote> votes){
+		// compute score
+		
+		// \sigma numEdges matched
+		// ----------------------
+		// numEdges * numFeatures
+		
+		int numEdgesMatched = 0;
+		int numEdges = 0;
+		for (int i = 0; i < g.numNodes(); i++) {
+			for (int j = i + 1; j < g.numNodes(); j++) {
+				if (g.isEdge(i, j)) {
+					numEdges++;
+					// What was my vote
+					for (Vote v : votes) {
+						if ((v.i == i && v.j == j) || (v.i2 == i && v.j2 == j)) {
+							for (Point p : axis) {
+								if (Math.abs(p.x() - v.theta) < xMin
+										&& Math.abs(p.y() - v.rad) < yMin) {
+									numEdgesMatched++;
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+		return numEdgesMatched / (double) (axis.size() * numEdges);
 	}
 }
