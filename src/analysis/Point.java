b318b045a4d2b912ed3cb6a61bae6eae519f89f2
Roman Klapaukh <roma@ecs.vuw.ac.nz>
2013-06-12 15:32:21 +1200

    Added methods to produce unit vectors in a given direction and vector add

--- src/analysis/Point.java
+++ src/analysis/Point.java
@@ -1,10 +1,10 @@
 package analysis;
 
 /**
- * This class represents a simple immutable 2D point. 
+ * This class represents a simple immutable 2D point.
  * It contains some basic geometric methods needed for the various kinds
  * of graph analysis that are preformed
- * 
+ *
  * @author Roma Klapaukh
  *
  */
@@ -23,8 +23,18 @@ public class Point {
 	}
 
 	/**
+	 * Make a new point pointing in a given direction with unit length
+	 *
+	 * @param theta Angle from x axis
+	 */
+	public Point(double theta){
+		x = Math.cos(theta);
+		y = Math.sin(theta);
+	}
+
+	/**
 	 * Make a point that has the same value as another point
-	 * 
+	 *
 	 * @param o The point to copy
 	 */
 	public Point(Point o){
@@ -45,9 +55,9 @@ public class Point {
 	/**
 	 * Makes this point represent a vector going up. If it is going backwards
 	 * it reverses it. This doesn't make sense for most points, but only does
-	 * when the point represents a direction that is the same forwards and 
+	 * when the point represents a direction that is the same forwards and
 	 * backwards.
-	 * 
+	 *
 	 * @return Either the same point, or this -this point.
 	 */
 	public Point forceForwards(){
@@ -75,7 +85,7 @@ public class Point {
 
 	/**
 	 * Vector subtraction. Returns a new point representing this - o.
-	 * 
+	 *
 	 * @param o Point to subtract from this.
 	 * @return
 	 */
@@ -85,27 +95,27 @@ public class Point {
 
 	/**
 	 * Computes the angle from this point to another.
-	 * The angle is measured from the x axis. 
+	 * The angle is measured from the x axis.
 	 * It returns a value in the range [0, Math.PI].
 	 * If this value from this to the other point is
 	 * outside that range, it will return the angle from
-	 * it to this point. 
-	 * 
+	 * it to this point.
+	 *
 	 * @param o The other point
 	 * @return The angle between points from the x axis
 	 */
 	public double angleToOtherFromXForward(Point o){
 		return this.minus(o).forceForwards().angleToX();
 	}
-	
+
 	public double angleToOtherFromX(Point o){
 		return this.minus(o).angleToX();
 	}
 
 	/**
-	 * Returns the angle between this point (treated as 
+	 * Returns the angle between this point (treated as
 	 * a vector direction) and the x axis.
-	 * 
+	 *
 	 * @return Angle to x axis
 	 */
 	public double angleToX(){
@@ -122,7 +132,7 @@ public class Point {
 	/**
 	 * Returns the norm of this vector. It is the distance
 	 * from the origin to here.
-	 * 
+	 *
 	 * @return Vector norm
 	 */
 	public double length(){
@@ -131,7 +141,7 @@ public class Point {
 
 	/**
 	 * Find the point exactly in this middle between this and o.
-	 * 
+	 *
 	 * @param o Other point to find center between
 	 * @return A point that is the center from this to o
 	 */
@@ -141,14 +151,30 @@ public class Point {
 
 	/**
 	 * Returns the same vector as this point but with unit length.
-	 * 
+	 *
 	 * @return The same direction with unit length
 	 */
 	public Point normalise(){
 		double length = this.length();
 		return new Point(this.x/ length, this.y / length);
 	}
-	
+
+	public Point rotate(double theta){
+		double nx = x*Math.cos(theta) - y * Math.sin(theta);
+		double ny = x*Math.sin(theta) + y * Math.cos(theta);
+
+		return new Point(nx, ny);
+	}
+
+	/**
+	 * Add two points
+	 * @param o The point to add
+	 * @return New point giving the vector sum
+	 */
+	public Point plus(Point o){
+		return new Point (this.x + o.x , this.y + o.y);
+	}
+
 	public String toString(){
 		return String.format("(%.2f, %.2f)", x, y);
 	}
