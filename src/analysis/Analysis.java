a41cc3c5fa19f92366a985b778472b2104f65e3b
Roman Klapaukh <roma@ecs.vuw.ac.nz>
2013-06-25 11:58:20 +1200

    Added constants for new outputs, and removed code for mean, sd, edge lengths and node distances

--- src/analysis/Analysis.java
+++ src/analysis/Analysis.java
@@ -1,8 +1,5 @@
 package analysis;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import main.Graph;
 
 public abstract class Analysis {
@@ -25,69 +22,11 @@ public abstract class Analysis {
 	public static final String WELLMASS = "wellMass";
 	public static final String EDGECHARGE = "edgeCharge";
 	public static final String FINALKINETICENERGY = "finalKineticEnergy";
+	public static final String NODEWIDTH = "nodeWidth";
+	public static final String NODEHEIGHT = "nodeHeight";
+	public static final String NODECHARGE = "nodeCharge";
 
 	public abstract String value(Graph g);
 
-	public double computeMean(List<Double> list){
-		if(list.isEmpty()) {
-			return 0;
-		}
-		double total = 0;
-		for(Double v : list){
-			total += v;
-		}
-		total /= list.size();
-		return total;
-	}
-
-	public double computeCorrectedStandardDeviation(List<Double> list){
-		if(list.isEmpty()){
-			return 0;
-		}
-		double total = 0;
-		double mean = computeMean(list);
-		for (Double val: list) {
-					total += Math.pow(val - mean, 2);
-		}
-		total = Math.sqrt(total / (double) (list.size()-1));
-		return total;
-	}
-
-	public List<Double> computeEdgeLengths(Graph g) {
-		List<Double> lengths = new ArrayList<>();
-		for (int i = 0; i < g.numNodes(); i++) {
-			for (int j = i; j < g.numNodes(); j++) {
-				if (g.isEdge(i, j)) {
-					lengths.add(g.distanceBetween(i, j));
-				}
-			}
-		}
-
-		//TODO This needs to replace the above once it has been tested
-		List<Double> tests = new ArrayList<>();
-		for(Pair<Point,Point> e : g.edgeIterator()){
-			tests.add(e.x.distanceTo(e.y));
-		}
-
-		if(tests.size() != lengths.size()){
-			throw new RuntimeException("Edge iterator is broken");
-		}
-		for(int i = 0 ; i < tests.size();i++){
-			if(Double.compare(tests.get(i), lengths.get(i)) != 0){
-				throw new RuntimeException("Edge iterator is broken");
-			}
-		}
-		return lengths;
-	}
-
-	public List<Double> computeVertexDistances(Graph g) {
-		List<Double> lengths = new ArrayList<>();
-		for (int i = 0; i < g.numNodes(); i++) {
-			for (int j = i + 1; j < g.numNodes(); j++) {
-				lengths.add(g.distanceBetween(i, j));
-			}
-		}
-		return lengths;
-	}
 
 }
