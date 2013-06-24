ce796da23b2c99425deb451b6cd7a1f5dfc59123
Roman Klapaukh <roma@ecs.vuw.ac.nz>
2013-06-25 11:58:38 +1200

    Find the width of the graph

---
+++ src/analysis/UsedHeight.java
@@ -0,0 +1,27 @@
+package analysis;
+
+import main.Graph;
+
+public class UsedHeight extends Analysis {
+
+	public String toString(){
+		return "Used Height";
+	}
+
+	@Override
+	public String value(Graph g) {
+		double min = Double.MAX_VALUE;
+		double max = Double.MIN_VALUE;
+
+		for(Point p : g){
+			if(p.y() < min){
+				min = p.y();
+			}
+			if(p.y() > max){
+				max = p.y();
+			}
+		}
+		return String.format("%.4f", max - min);
+	}
+
+}
