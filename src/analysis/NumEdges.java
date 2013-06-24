d951a1ecb69e87fee4313b6f72639ebd2222ad0f
Roman Klapaukh <roma@ecs.vuw.ac.nz>
2013-06-25 11:54:51 +1200

    Only using edge iterator now

--- src/analysis/NumEdges.java
+++ src/analysis/NumEdges.java
@@ -4,7 +4,7 @@ import main.Graph;
 
 public class NumEdges extends Analysis {
 
-	public String toString(){
+	public String toString() {
 		return "Num Edges";
 	}
 
@@ -12,21 +12,11 @@ public class NumEdges extends Analysis {
 	public String value(Graph g) {
 		int numEdges = 0;
 
-		for(@SuppressWarnings("unused") Pair<Point,Point> p : g.edgeIterator()){
+		for (@SuppressWarnings("unused")
+		Pair<Point, Point> p : g.edgeIterator()) {
 			numEdges += 1;
 		}
 
-		//TODO This needs to be removed once it has been tested more!!
-		int total = 0;
-		for(int i = 0 ; i < g.numNodes() ; i++){
-			for(int j = i; j < g.numNodes(); j++){
-				total += g.isEdge(i, j)? 1 : 0;
-			}
-		}
-
-		if(numEdges != total){
-			throw new RuntimeException("Edge counting failed " + numEdges + " -- " + total);
-		}
 		return String.format("%d", numEdges);
 	}
 
