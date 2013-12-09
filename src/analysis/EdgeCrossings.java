836c58500eb88420144048705758c6b85caacffb
Roman Klapaukh <roma@ecs.vuw.ac.nz>
2013-12-09 14:19:15 +1300

    More bugs in edge crossingsgit diff

--- src/analysis/EdgeCrossings.java
+++ src/analysis/EdgeCrossings.java
@@ -30,6 +30,7 @@ public class EdgeCrossings extends Analysis {
 	}
 
 	public String value(Graph g) {
+//		System.out.println();
 		int count = 0;
 		for (int i = 0; i < g.numNodes(); i++) {
 			for (int j = i + 1; j < g.numNodes(); j++) {
@@ -37,58 +38,99 @@ public class EdgeCrossings extends Analysis {
 					for (int k = i + 1; k < g.numNodes(); k++) {
 						for (int l = k + 1; l < g.numNodes(); l++) {
 							if (g.isEdge(k, l)) {
-								// Disconnected
-								Point p1 = g.getNode(i);
-								Point p2 = g.getNode(j);
+								if (k == i || l == i || k == j || l == j) {
+									// Two edges going out from i
+									// Change the names so the graph looks like |a| -- |b| -- |c|
+									int a, b, c;
+									if (k == i) {
+										a = j;
+										b = i;
+										c = l;
+									} else if (k == j) {
+										a = i;
+										b = j;
+										c = l;
+									} else if (l == i) {
+										a = j;
+										b = i;
+										c = k;
+									} else if (l == j) {
+										a = i;
+										b = j;
+										c = k;
+									} else {
+										throw new RuntimeException("Doubled up node not found");
+									}
 
-								Point p3 = g.getNode(k);
-								Point p4 = g.getNode(l);
+									// They overlap if they have the same gradient going out of b!
+									Point pa = g.getNode(a);
+									Point pb = g.getNode(b);
+									Point pc = g.getNode(c);
 
-								/*
-								 * [p4-3.x p1-2.x][t] = [p1-3.x] [p4-3.y p1-2.y][s] = [p1-3.y]
-								 */
-								// Check if matrix is solvable
-								double a = p4.x() - p3.x();
-								double b = p1.x() - p2.x();
-								double c = p4.y() - p3.y();
-								double d = p1.y() - p2.y();
+									double gradba = (pa.y() - pb.y()) / (pa.x() - pb.x());
+									double gradbc = (pc.y() - pb.y()) / (pc.x() - pb.x());
 
-								double e = p1.x() - p3.x();
-								double f = p1.y() - p3.y();
-
-								double det = a * d - b * c;
-
-								if (Double.compare(det, 0) != 0) {
-									double s = (d * e - b * f) / det;
-									double t = (-c * e + a * f) / det;
-
-									if (s >= 0 && s <= 1 && t >= 0 && t <= 1) {
+									if (Double.compare(gradba, gradbc) == 0) {
+//										System.out.printf("%d -- %d -- %d", a,b,c);
 										count += 1;
 									}
 								} else {
-									// Det (Matrix) = 0
-									// Their are either infinite solutions or none
+									// Disconnected
+									Point p1 = g.getNode(i);
+									Point p2 = g.getNode(j);
 
-									// check for none -- one line never becomes the other
-									double gx = a;
-									double gy = c;
+									Point p3 = g.getNode(k);
+									Point p4 = g.getNode(l);
 
-									// /gx and gy can only be zero if the edge has zero length
-									if (Double.compare(gx, 0) == 0 && Double.compare(gy, 0) == 0) {
-										// There can't really be an edge crossing, as this edge has 0 length
-										continue;
-									} else if (Double.compare(p1.x() / gx, p1.y() / gy) == 0) {
-										// They might overlap
-										double tStart = p1.x() / gx;
-										double tEnd = p2.x() / gx;
+									/*
+									 * [p4-3.x p1-2.x][t] = [p1-3.x] [p4-3.y p1-2.y][s] = [p1-3.y]
+									 */
+									// Check if matrix is solvable
+									double a = p4.x() - p3.x();
+									double b = p1.x() - p2.x();
+									double c = p4.y() - p3.y();
+									double d = p1.y() - p2.y();
 
-										if (tStart >= 0 && tStart <= 1) {
+									double e = p1.x() - p3.x();
+									double f = p1.y() - p3.y();
+
+									double det = a * d - b * c;
+
+									if (Double.compare(det, 0) != 0) {
+										double s = (d * e - b * f) / det;
+										double t = (-c * e + a * f) / det;
+
+										if (s >= 0 && s <= 1 && t >= 0 && t <= 1) {
 											count += 1;
-										} else if (tEnd >= 0 && tEnd <= 1) {
-											count += 1;
-										} else if (Math.min(tEnd, tStart) <= 0 && Math.max(tEnd, tStart) >= 1) {
+//											System.out.printf("%d -- %d && %d -- %d\n", i,j,k,l);
 										}
-									}// else { They never overlap }
+									} else {
+										// Det (Matrix) = 0
+										// Their are either infinite solutions or none
+
+										// check for none -- one line never becomes the other
+										double gx = a;
+										double gy = c;
+
+										// /gx and gy can only be zero if the edge has zero length
+										if (Double.compare(gx, 0) == 0 && Double.compare(gy, 0) == 0) {
+											// There can't really be an edge crossing, as this edge has 0 length
+											continue;
+										} else if (Double.compare(p1.x() / gx, p1.y() / gy) == 0) {
+											// They might overlap
+											double tStart = p1.x() / gx;
+											double tEnd = p2.x() / gx;
+
+											if (tStart >= 0 && tStart <= 1) {
+												count += 1;
+												System.out.printf("%d -- %d && %d -- %d\n", i,j,k,l);
+											} else if (tEnd >= 0 && tEnd <= 1) {
+												count += 1;
+												System.out.printf("%d -- %d && %d -- %d\n", i,j,k,l);
+											} else if (Math.min(tEnd, tStart) <= 0 && Math.max(tEnd, tStart) >= 1) {
+											}
+										}// else { They never overlap }
+									}
 								}
 							}
 						}
